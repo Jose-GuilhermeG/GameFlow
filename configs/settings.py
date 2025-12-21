bfbd59153dea3e5515cd20c9588a1df487bd8df6
Guilherme <jg21504956@gmail.com>
2025-12-21 15:34:19 -0300

    Adicionando a estrutura basica do django

---
+++ configs/settings.py
@@ -0,0 +1,108 @@
+from pathlib import Path
+from os.path import join
+from environ import Env
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+Env.read_env(join(BASE_DIR, ".env"))
+
+env = Env()
+SECRET_KEY = env("SECRET_KEY")
+DEBUG = env.bool("DEBUG", default=False)
+ALLOWED_HOSTS = ["*"]
+TRIDY_APPS = [
+
+]
+PROJECT_APPS = [
+    "users",
+    "core",
+]
+DJANGO_APPS = [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "django.contrib.sites",
+]
+INSTALLED_APPS = DJANGO_APPS + TRIDY_APPS + PROJECT_APPS
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+ROOT_URLCONF = "configs.urls"
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
+WSGI_APPLICATION = "configs.wsgi.application"
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": env("DATABASE_NAME"),
+        "USER": env("DATABASE_USER"),
+        "HOST": env("DATABASE_HOST"),
+        "PASSWORD": env("DATABASE_PASSWORD"),
+        "PORT": env("DATABASE_PORT"),
+        "ATOMIC_REQUESTS": True,
+        "TEST": {
+            "NAME": "gameFlow_test",
+            "CHARSET": "utf8mb4",
+            "COLLATION": "utf8mb4_unicode_ci",
+        },
+    }
+}
+FIXTURE_DIRS = [
+    BASE_DIR / "fixtures",
+]
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+    },
+]
+PASSWORD_HASHERS = [
+    "django.contrib.auth.hashers.BCryptSHA256PasswordHasher",
+    "django.contrib.auth.hashers.PBKDF2PasswordHasher",
+    "django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher",
+    "django.contrib.auth.hashers.Argon2PasswordHasher",
+    "django.contrib.auth.hashers.ScryptPasswordHasher",
+]
+LANGUAGE_CODE = "en-us"
+TIME_ZONE = "UTC"
+USE_I18N = True
+USE_TZ = True
+LENGUAGES = [
+    ("en", "English"),
+    ("pt-br", "Português"),
+]
+LOCALE_PATHS = [join(BASE_DIR, "locale")]
+STATIC_URL = "/static/"
+STATIC_ROOT = join(BASE_DIR, "static")
+MEDIA_ROOT = join(BASE_DIR, "media")
+MEDIA_URL = "/media/"
